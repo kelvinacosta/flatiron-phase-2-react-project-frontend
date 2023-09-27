22cef3cd58665c649302f0a39ec0d00de7fc498d
Kelvin Acosta <acosta.kelvin02@gmail.com>
2023-09-27 19:59:07 -0400

    create a header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,25 @@
+const Header = () => {
+    return (
+        <>
+        <header>
+        <h1>My App</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#some-id1">Home</a>
+                </li>
+                <li>
+                    <a href="#some-id2">About</a>
+                </li>
+                <li>
+                    <a href="#some-id3">Form</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
+        
+        </>
+    )
+}
+
+export default Header;
